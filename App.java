5aeb328e5f297583804ccb577aa54db4812111d6
Vaishnavi Kesherwani <122589971+Vaishu7605@users.noreply.github.com>
2024-08-05 16:56:19 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+package vaishnavi.meaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
